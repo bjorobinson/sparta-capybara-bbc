6cb1ba41341d48775829a1dc31397ec3328ce5eb
Tech-A12 <tech-a12@android-2da98d21d4cff79c.home>
2018-03-27 21:31:17 +0100

    Date of Birth input options partially tested. Full testing would require testing every individual combination of invalid date as each return its own error message.

--- spec/bbc_login_error_spec.rb
+++ spec/bbc_login_error_spec.rb
@@ -47,7 +47,7 @@ end
 
 describe "Full Registration Process" do
   context "It should return the correct errors on invalid inputs throughout the registration process" do
-    it "should return an error on the date of birth page when day given is not a valid input" do
+    it "should return correct error on the date of birth page when day given is not a valid number" do
       @bbc_site = BBC_Site.new
       @bbc_site.bbc_homepage.visit_home_page
       @bbc_site.bbc_homepage.click_sign_in_link
@@ -59,5 +59,45 @@ describe "Full Registration Process" do
       @bbc_site.bbc_dob_page.select_submit_link
       expect(@bbc_site.bbc_dob_page.get_dob_error_msg).to eq "Oops, that day doesn't look right. Make sure it's a real date written as DD-MM-YYYY e.g. the 5th of June 2009 is 05-06-2009."
     end
+
+    it "should correct error on the date of birth page when month given is not a valid number" do
+      @bbc_site = BBC_Site.new
+      @bbc_site.bbc_homepage.visit_home_page
+      @bbc_site.bbc_homepage.click_sign_in_link
+      @bbc_site.bbc_login.click_sign_up_link
+      @bbc_site.bbc_age_page.select_13_and_over
+      @bbc_site.bbc_dob_page.set_day_value "6"
+      @bbc_site.bbc_dob_page.set_month_value "Q"
+      @bbc_site.bbc_dob_page.set_year_value "1996"
+      @bbc_site.bbc_dob_page.select_submit_link
+      expect(@bbc_site.bbc_dob_page.get_dob_error_msg).to eq "Oops, that month doesn't look right. Make sure it's a real date written as DD-MM-YYYY e.g. the 5th of June 2009 is 05-06-2009."
+    end
+
+    it "should return an error on the date of birth page when day given is not a valid number" do
+      @bbc_site = BBC_Site.new
+      @bbc_site.bbc_homepage.visit_home_page
+      @bbc_site.bbc_homepage.click_sign_in_link
+      @bbc_site.bbc_login.click_sign_up_link
+      @bbc_site.bbc_age_page.select_13_and_over
+      @bbc_site.bbc_dob_page.set_day_value "6"
+      @bbc_site.bbc_dob_page.set_month_value "7"
+      @bbc_site.bbc_dob_page.set_year_value "Q"
+      @bbc_site.bbc_dob_page.select_submit_link
+      expect(@bbc_site.bbc_dob_page.get_dob_error_msg).to eq "Oops, that year doesn't look right. Make sure it's a real date written as DD-MM-YYYY e.g. the 5th of June 2009 is 05-06-2009."
+    end
+
+    it "should return an error on the date of birth page when year given is in the future" do
+      @bbc_site = BBC_Site.new
+      @bbc_site.bbc_homepage.visit_home_page
+      @bbc_site.bbc_homepage.click_sign_in_link
+      @bbc_site.bbc_login.click_sign_up_link
+      @bbc_site.bbc_age_page.select_13_and_over
+      @bbc_site.bbc_dob_page.set_day_value "6"
+      @bbc_site.bbc_dob_page.set_month_value "7"
+      @bbc_site.bbc_dob_page.set_year_value "2023"
+      @bbc_site.bbc_dob_page.select_submit_link
+      expect(@bbc_site.bbc_dob_page.get_dob_error_msg).to eq "Oops, that date's in the future. Make sure it’s a date in the past. (Unless you’re a time traveller. In which case, what’s the future like? Are there flying cars?)"
+    end
+
   end
 end

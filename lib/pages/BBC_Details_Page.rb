375a13c6d86b83ad0ee79534ca2d6c7f947bf848
Tech-A12 <tech-a12@android-2da98d21d4cff79c.home>
2018-03-27 22:34:38 +0100

    Implemented and tested possible outcomes for password errors

--- lib/pages/BBC_Details_Page.rb
+++ lib/pages/BBC_Details_Page.rb
@@ -4,8 +4,23 @@ class BBC_Details_Page
   include Capybara::DSL
 
   EMAIL_ID = "#email"
+  PASSWORD_VALUE_NAME = "password"
+  PASSWORD_ERROR_MSG = "#form-message-password"
+  SUBMIT_BUTTON_ID = "#submit-button"
 
   def find_email
     find(EMAIL_ID)['id']
+  end
+
+  def submit_form
+    find(SUBMIT_BUTTON_ID).click
+  end
+
+  def set_password_value new_password
+    fill_in PASSWORD_VALUE_NAME, with: new_password
+  end
+
+  def get_password_error_msg
+    find(PASSWORD_ERROR_MSG).text
   end
 end
--- spec/bbc_login_error_spec.rb
+++ spec/bbc_login_error_spec.rb
@@ -47,44 +47,85 @@ end
 
 describe "Full Registration Process" do
   context "It should return the correct errors on invalid inputs on Date of Birth Page" do
+    # before(:each) do
+    #   @bbc_site = BBC_Site.new
+    #   @bbc_site.bbc_homepage.visit_home_page
+    #   @bbc_site.bbc_homepage.click_sign_in_link
+    #   @bbc_site.bbc_login.click_sign_up_link
+    #   @bbc_site.bbc_age_page.select_13_and_over
+    # end
+    #
+    # it "when day given is not a valid number" do
+    #   @bbc_site.bbc_dob_page.set_dob_values ["Q","7","1996"]
+    #   @bbc_site.bbc_dob_page.select_submit_link
+    #   expect(@bbc_site.bbc_dob_page.get_dob_error_msg).to eq "Oops, that day doesn't look right. Make sure it's a real date written as DD-MM-YYYY e.g. the 5th of June 2009 is 05-06-2009."
+    # end
+    #
+    # it "when month given is not a valid number" do
+    #   @bbc_site.bbc_dob_page.set_dob_values ["6","Q","1996"]
+    #   @bbc_site.bbc_dob_page.select_submit_link
+    #   expect(@bbc_site.bbc_dob_page.get_dob_error_msg).to eq "Oops, that month doesn't look right. Make sure it's a real date written as DD-MM-YYYY e.g. the 5th of June 2009 is 05-06-2009."
+    # end
+    #
+    # it "when year given is not a valid number" do
+    #   @bbc_site.bbc_dob_page.set_dob_values ["6","7","Q"]
+    #   @bbc_site.bbc_dob_page.select_submit_link
+    #   expect(@bbc_site.bbc_dob_page.get_dob_error_msg).to eq "Oops, that year doesn't look right. Make sure it's a real date written as DD-MM-YYYY e.g. the 5th of June 2009 is 05-06-2009."
+    # end
+    #
+    # it "when year given is in the future" do
+    #   @bbc_site.bbc_dob_page.set_dob_values ["6","7","2023"]
+    #   @bbc_site.bbc_dob_page.select_submit_link
+    #   expect(@bbc_site.bbc_dob_page.get_dob_error_msg).to eq "Oops, that date's in the future. Make sure it’s a date in the past. (Unless you’re a time traveller. In which case, what’s the future like? Are there flying cars?)"
+    # end
+    #
+    # it "should return no error message and move to the next part of the signup when valid numbers for date of birth are provided" do
+    #   @bbc_site.bbc_dob_page.set_dob_values ["6","7","1996"]
+    #   @bbc_site.bbc_dob_page.select_submit_link
+    #   # Test to find if bot has landed on the first page - tries to find the email field. Admittedly this is an arbitrary check
+    #   expect(@bbc_site.bbc_details_page.find_email).to eq 'email'
+    # end
+  end
+
+  context "It should return the correct errors on invalid inputs on Details Page" do
     before(:each) do
       @bbc_site = BBC_Site.new
-        @bbc_site.bbc_homepage.visit_home_page
-        @bbc_site.bbc_homepage.click_sign_in_link
-        @bbc_site.bbc_login.click_sign_up_link
-        @bbc_site.bbc_age_page.select_13_and_over
-    end
-
-    it "should return correct error on the date of birth page when day given is not a valid number" do
-      @bbc_site.bbc_dob_page.set_dob_values ["Q","7","1996"]
-      @bbc_site.bbc_dob_page.select_submit_link
-      expect(@bbc_site.bbc_dob_page.get_dob_error_msg).to eq "Oops, that day doesn't look right. Make sure it's a real date written as DD-MM-YYYY e.g. the 5th of June 2009 is 05-06-2009."
-    end
-
-    it "should correct error on the date of birth page when month given is not a valid number" do
-      @bbc_site.bbc_dob_page.set_dob_values ["6","Q","1996"]
-      @bbc_site.bbc_dob_page.select_submit_link
-      expect(@bbc_site.bbc_dob_page.get_dob_error_msg).to eq "Oops, that month doesn't look right. Make sure it's a real date written as DD-MM-YYYY e.g. the 5th of June 2009 is 05-06-2009."
-    end
-
-    it "should return an error on the date of birth page when day given is not a valid number" do
-      @bbc_site.bbc_dob_page.set_dob_values ["6","7","Q"]
-      @bbc_site.bbc_dob_page.select_submit_link
-      expect(@bbc_site.bbc_dob_page.get_dob_error_msg).to eq "Oops, that year doesn't look right. Make sure it's a real date written as DD-MM-YYYY e.g. the 5th of June 2009 is 05-06-2009."
-    end
-
-    it "should return an error on the date of birth page when year given is in the future" do
-      @bbc_site.bbc_dob_page.set_dob_values ["6","7","2023"]
-      @bbc_site.bbc_dob_page.select_submit_link
-      expect(@bbc_site.bbc_dob_page.get_dob_error_msg).to eq "Oops, that date's in the future. Make sure it’s a date in the past. (Unless you’re a time traveller. In which case, what’s the future like? Are there flying cars?)"
-    end
-
-    it "should return no error message and move to the next part of the signup when valid numbers for date of birth are provided" do
+      @bbc_site.bbc_homepage.visit_home_page
+      @bbc_site.bbc_homepage.click_sign_in_link
+      @bbc_site.bbc_login.click_sign_up_link
+      @bbc_site.bbc_age_page.select_13_and_over
       @bbc_site.bbc_dob_page.set_dob_values ["6","7","1996"]
       @bbc_site.bbc_dob_page.select_submit_link
-      # Test to find if bot has landed on the first page - tries to find the email field. Admittedly this is an arbitrary check
-      expect(@bbc_site.bbc_details_page.find_email).to eq 'email'
     end
 
+    it "when password field is empty" do
+      @bbc_site.bbc_details_page.set_password_value " "
+      @bbc_site.bbc_details_page.submit_form
+      expect(@bbc_site.bbc_details_page.get_password_error_msg).to eq "Sorry, that password is too short. It needs to be eight characters or more."
+    end
+
+    it "when password is too short" do
+      @bbc_site.bbc_details_page.set_password_value "pass"
+      @bbc_site.bbc_details_page.submit_form
+      expect(@bbc_site.bbc_details_page.get_password_error_msg).to eq "Sorry, that password is too short. It needs to be eight characters or more."
+    end
+
+    it "when password doesn't contain a letter" do
+      @bbc_site.bbc_details_page.set_password_value "12345678"
+      @bbc_site.bbc_details_page.submit_form
+      expect(@bbc_site.bbc_details_page.get_password_error_msg).to eq "Sorry, that password isn't valid. Please include a letter."
+    end
+
+    it "when password only contains letters" do
+      @bbc_site.bbc_details_page.set_password_value "password"
+      @bbc_site.bbc_details_page.submit_form
+      expect(@bbc_site.bbc_details_page.get_password_error_msg).to eq "Sorry, that password isn't valid. Please include something that isn't a letter."
+    end
+
+    it "when password is easy to guess" do
+      @bbc_site.bbc_details_page.set_password_value "password1"
+      @bbc_site.bbc_details_page.submit_form
+      expect(@bbc_site.bbc_details_page.get_password_error_msg).to eq "Sorry, that password isn't valid. Make sure it's hard to guess."
+    end
   end
 end
